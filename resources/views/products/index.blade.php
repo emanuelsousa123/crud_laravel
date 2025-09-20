a5b37372854c8bacc7a569ba6c15cc5f83013e3e
EmanuelRC <emanuel.sousa99@aluno.ce.gov.br>
2025-09-20 07:46:21 -0300

    definida a view para receber os valores do bd

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,6 +6,11 @@
     <title>Document</title>
 </head>
 <body>
-    hello world
+    <h1>Products</h1>
+    <ul>
+        @foreach ($products as $product)
+            <li>{{ $product->name }}</li>
+        @endforeach
+    </ul>
 </body>
 </html>
